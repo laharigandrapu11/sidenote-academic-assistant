04847237f29173a842d0bca0ccf6de169b387043
Lahari Gandrapu <laharigandrapu11@gmail.com>
2025-05-26 19:13:45 -0400

    Delete sidenote directory

--- sidenote/utils/pdf_parser.py
+++
@@ -1,8 +0,0 @@
-import fitz  # PyMuPDF
-
-def extract_text_from_pdf(uploaded_file):
-    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
-    full_text = ""
-    for page in doc:
-        full_text += page.get_text()
-    return full_text
